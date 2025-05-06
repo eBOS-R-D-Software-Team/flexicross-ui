a480963a944a604bd67b00922f667951ec49ed1f
slimt <slimt@ebos.com.cy>
2025-05-06 03:03:13 +0100

    remove slash

--- src/redux/slices/detectionSlice.ts
+++ src/redux/slices/detectionSlice.ts
@@ -8,7 +8,7 @@ const isNetlify = process.env.REACT_APP_NETLIFY == 'true';  // make sure your en
 // choose the base for all your calls
 const API_BASE = isNetlify
   // in prod your Netlify redirects will proxy /api → http://172.16.11.12:8080/api
-  ? '/api'
+  ? 'api'
   // in local/dev go directly to your backend
   : 'http://172.16.11.12:8080/api';
 
