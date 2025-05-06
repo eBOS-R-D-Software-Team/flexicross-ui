616d0b352a1432594f67e1e08fcfdef1a08f399f
slimt <slimt@ebos.com.cy>
2025-05-06 02:28:50 +0100

    keycloak changes

--- src/keycloak.js
+++ src/keycloak.js
@@ -2,7 +2,9 @@ import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
     realm: process.env.REACT_APP_REALM_NAME,
-    url: process.env.REACT_APP_KEYCLOAK_URL,
+    url: process.env.REACT_APP_NETLIFY == 'true'
+    ? '/keycloak'
+    : process.env.REACT_APP_KEYCLOAK_URL,
     clientId: process.env.REACT_APP_CLIENT_ID
 });
 
