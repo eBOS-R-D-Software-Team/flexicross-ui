04b290db86aed686f283422046a9e08de84e2827
slimt <slimt@ebos.com.cy>
2025-05-06 02:39:53 +0100

    remove keycloak redirection

--- src/keycloak.js
+++ src/keycloak.js
@@ -2,9 +2,7 @@ import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
     realm: process.env.REACT_APP_REALM_NAME,
-    url: process.env.REACT_APP_NETLIFY == 'true'
-    ? '/keycloak'
-    : process.env.REACT_APP_KEYCLOAK_URL,
+    url: process.env.REACT_APP_KEYCLOAK_URL,
     clientId: process.env.REACT_APP_CLIENT_ID
 });
 
